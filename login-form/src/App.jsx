2e2ec7ffdbbaf961eff0a6407a8bb5ea0980aaa6
Shreya Kalra <Shreyaa.kalra@gmail.com>
2025-12-10 04:49:39 +0530

    made a login page in react

---
+++ login-form/src/App.jsx
@@ -0,0 +1,44 @@
+
+import { useState } from 'react'
+
+export function LoginForm() {
+        const [showPassword, setShowPassword] = useState(false);
+
+        function toggleShowPassword() {
+          setShowPassword(!showPassword);
+        }
+
+        return (
+          <>
+            <div>
+              <input
+                placeholder="Email"
+                className="login-input"
+              />
+            </div>
+            <div>
+              <input
+                placeholder="Password"
+                type={showPassword ? 'text' : 'password'}
+                className="login-input"
+              />
+              <button
+                className="show-button"
+                onClick={toggleShowPassword}
+              >
+                {showPassword ? 'Hide' : 'Show'}
+              </button>
+            </div>
+            <button className="login-button">Login</button>
+            <button className="login-button">Sign up</button>
+          </>
+        );
+      }
+      export default function App() {
+        return (
+          <>
+            <p className="title">Hello, welcome to my website</p>
+            <LoginForm />
+          </>
+        );
+      }
