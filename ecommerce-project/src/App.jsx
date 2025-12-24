eb6447d78a74261406a669260c242e34b180d35a
Shreya Kalra <Shreyaa.kalra@gmail.com>
2025-12-24 06:12:38 +0530

    structerized things

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,13 @@
 // import { useState } from 'react'
+import { HomePage } from './pages/Homepage';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      <button>HELLO</button>
+      <HomePage />
     </>
   )
 }
