0434e821cc58355b4aa2aa032f3b4b4d68719320
Shreya Kalra <Shreyaa.kalra@gmail.com>
2025-12-28 06:00:30 +0530

    added orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 import { HomePage } from './pages/Homepage';
 import { CheckoutPage } from './pages/Checkoutpage';
+import { Orders } from './pages/Orders';
 import './App.css'
 
 function App(){
@@ -9,6 +10,7 @@ function App(){
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<Orders />} />
       </Routes>
       
     </>
