24956c7caece14568ecdd65ca05804bcdaf75036
Shreya Kalra <Shreyaa.kalra@gmail.com>
2025-12-28 05:53:10 +0530

    changed links

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -8,7 +8,7 @@ function App(){
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="checkout.html" element={<CheckoutPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
       
     </>
