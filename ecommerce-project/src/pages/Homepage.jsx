03572b6f24767d775aae5216ff49820907c6ed32
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-01-04 03:24:38 +0530

    used data from the backend to generate html

--- ecommerce-project/src/pages/Homepage.jsx
+++ ecommerce-project/src/pages/Homepage.jsx
@@ -1,15 +1,18 @@
 import { Header } from "../components/Headers.jsx";
+import {useEffect, useState} from 'react';
 import axios from 'axios';
-import { products } from "../../starting-code/data/products.js";
 import "./Homepage.css";
 
 export function HomePage() {
 
-  axios.get('http://localhost:3000/api/products')
-    .then((response) => {
-        console.log(response.data);
-      });
+  const [products, setProducts] = useState([])
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response) => {
+          setProducts(response.data);
+        });
+  },[])
 
   return (
     <>
